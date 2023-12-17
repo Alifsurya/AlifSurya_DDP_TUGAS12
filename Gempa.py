80d56512203e92e9bc5544ea670acf0b2b5ff4f4
Alifsurya <146050995+Alifsurya@users.noreply.github.com>
2023-12-17 17:52:10 +0700

    Add files via upload

---
+++ Gempa.py
@@ -0,0 +1,23 @@
+class Gempa:
+#member1 variabel
+    lok = ""
+    scale = 0
+    def __init__(self,lokasi,skala):
+        self.lok = lokasi
+        self.scale = skala
+    def dampak(self):
+        if self.scale < 2:
+            return "dampak gempa tidak berasa"
+        elif self.scale <= 4:
+            return "dampak gempa bangunan retak-retak"
+        elif self.scale <= 6:
+            return "dampak gempa bangunan roboh"
+        elif self.scale <= 10:
+            return "dampak gempa bangunan roboh dan berpotensi tsunami"
+        else:
+            return "Data tidak valid"
+    def cetak(self):
+        print("lokasi\t:",self.lok,
+        "\nskala\t:",self.scale,
+        "\ndampak\t:",self.dampak(),
+        "\n-------------------------------------------------------")
---
+++ tempat.py
@@ -0,0 +1,18 @@
+from Gempa import *
+#ciptakan object
+kejadian1= Gempa("Banten",1.2)
+kejadian2 = Gempa("Palu",6.1)
+kejadian3 = Gempa("Cianjur",5.6)
+kejadian4= Gempa("Jayapura",3.3)
+kejadian5 = Gempa("Garut",4.0)
+
+kejadian1.dampak()
+kejadian1.cetak()
+kejadian2.dampak()
+kejadian2.cetak()
+kejadian3.dampak()
+kejadian3.cetak()
+kejadian4.dampak()
+kejadian4.cetak()
+kejadian5.dampak()
+kejadian5.cetak()
